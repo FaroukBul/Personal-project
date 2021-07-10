ac802a72747dda0352d7d31392f636c5d6015524
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-07-10 15:12:20 -0500

    Added functions for diagonal winner and refactored to make all functions work on any grid size

--- scripts/main.js
+++ scripts/main.js
@@ -55,12 +55,13 @@ document.addEventListener('DOMContentLoaded', () => {
     function checkForWinner(){
         redCells = document.getElementsByClassName("red")
         blueCells = document.getElementsByClassName("blue")
-        winner = checkForRedWinner()
+        winner = checkForLineWinner()
+        checkForDiagonalWinner()
         return winner
     }
 
 
-    function checkForRedWinner(){
+    function checkForLineWinner(){
         grid = gridRange(gridSize)
         for(i=grid[0]; i<grid[1]; i++){
             completeRedRow = 0
@@ -85,6 +86,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     }
+
+    function checkForDiagonalWinner(){
+        if(!checkForRightDiagonal()){
+            checkForLeftDiagonal()
+        }
+    }
+
+
+    function checkForRightDiagonal(){
+        grid = gridRange(gridSize)
+        let y = 0
+        rightRedDiagonal = 0
+        rightBlueDiagonal = 0
+        for(x=grid[0]; x<grid[1]; x += 1){
+            y++
+            diagonal = document.getElementById(`${x}${y}`)
+            if(diagonal.classList.contains('red')){rightRedDiagonal += 1}
+            if(diagonal.classList.contains('blue')){rightBlueDiagonal += 1}
+        }
+        if(rightRedDiagonal == gridSize){
+            alert("Red Wins")
+            return location.reload()
+        } else if(rightBlueDiagonal == gridSize){
+            alert("Blue Wins")
+            return location.reload()
+        }
+
+        return null
+    }
+
+    function checkForLeftDiagonal(){
+        grid = gridRange(gridSize)
+        y=0
+        leftRedDiagonal = 0
+        leftBlueDiagonal = 0
+        for(x=gridSize; x>0; x -= 1){
+            y++
+            diagonal = document.getElementById(`${x}${y}`)
+            console.log(diagonal, x, y, "hello")
+            if(diagonal.classList.contains('red')){leftRedDiagonal += 1}
+            if(diagonal.classList.contains('blue')){leftBlueDiagonal += 1}
+        }
+        if(leftRedDiagonal == gridSize){
+            alert("Red Wins")
+            return location.reload()
+        } else if(leftBlueDiagonal == gridSize){
+            alert("Blue Wins")
+            return location.reload()
+        }
+    }
+
+
+    
 
 })
 
