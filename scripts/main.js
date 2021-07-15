a3c1c1a7096e84b1411911e3518ee0c471392678
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-07-14 20:11:54 -0500

    Added images to the cells when clicked, nav bar and correct variable declarations

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     let gridSize = 3
     createGrid(gridSize)
-    redCells = document.getElementsByClassName("red")
+    let redCells = document.getElementsByClassName("red")
     if(redCells.length == 0){
-        player = "initial"
+        var player = "initial"
     }
     function gridRange(length){
         return [1, length + 1]
     }
     function createGrid(gridSize){
         var gameGrid = document.getElementById("grid")
-        grid = gridRange(gridSize)
-        for(x=grid[0]; x<grid[1]; x++){
-            for(y=grid[0]; y<grid[1]; y++){
-                div = document.createElement("div")
+        let grid = gridRange(gridSize)
+        for(let x=grid[0]; x<grid[1]; x++){
+            for(let y=grid[0]; y<grid[1]; y++){
+                let div = document.createElement("div")
                 div.id = `${y}${x}`
                 div.setAttribute("class", "cell")
                 gameGrid.appendChild(div)
@@ -66,15 +66,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function checkForLineWinner(){
-        grid = gridRange(gridSize)
-        for(i=grid[0]; i<grid[1]; i++){
-            completeRedRow = 0
-            completeRedColumn = 0
-            completeBlueRow = 0
-            completeBlueColumn = 0
-            for(j=grid[0]; j<grid[1]; j++){
-                rowCell = document.getElementById(`${i}${j}`)
-                columnCell = document.getElementById(`${j}${i}`)
+        let grid = gridRange(gridSize)
+        for(let i=grid[0]; i<grid[1]; i++){
+            let completeRedRow = 0
+            let completeRedColumn = 0
+            let completeBlueRow = 0
+            let completeBlueColumn = 0
+            for(let j=grid[0]; j<grid[1]; j++){
+                let rowCell = document.getElementById(`${i}${j}`)
+                let columnCell = document.getElementById(`${j}${i}`)
                 if(rowCell.classList.contains('red')){completeRedRow += 1}
                 if(rowCell.classList.contains('blue')){completeBlueRow += 1}
                 if(columnCell.classList.contains('red')){completeRedColumn += 1}
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function checkForDiagonalWinner(){
-        winner = checkForRightDiagonal()
+        let winner = checkForRightDiagonal()
         if(!winner){
             winner = checkForLeftDiagonal()
         }
@@ -101,11 +101,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function checkForRightDiagonal(){
-        grid = gridRange(gridSize)
+        let grid = gridRange(gridSize)
         let y = 0
-        rightRedDiagonal = 0
-        rightBlueDiagonal = 0
-        for(x=grid[0]; x<grid[1]; x += 1){
+        let rightRedDiagonal = 0
+        let rightBlueDiagonal = 0
+        for(let x=grid[0]; x<grid[1]; x += 1){
             y++
             diagonal = document.getElementById(`${x}${y}`)
             if(diagonal.classList.contains('red')){rightRedDiagonal += 1}
@@ -121,11 +121,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function checkForLeftDiagonal(){
-        grid = gridRange(gridSize)
-        y=0
-        leftRedDiagonal = 0
-        leftBlueDiagonal = 0
-        for(x=gridSize; x>0; x -= 1){
+        let grid = gridRange(gridSize)
+        let y=0
+        let leftRedDiagonal = 0 
+        let leftBlueDiagonal = 0
+        for(let x=gridSize; x>0; x -= 1){
             y++
             diagonal = document.getElementById(`${x}${y}`)
             if(diagonal.classList.contains('red')){leftRedDiagonal += 1}
@@ -142,15 +142,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function determineGameOver(playerOneCells){
-        playerOneMoves = playerOneCells.length
+        let playerOneMoves = playerOneCells.length
         if(gridSize % 2 == 0){
-            maxMoves = (gridSize * gridSize)/2 + 1
+           let  maxMoves = (gridSize * gridSize)/2 + 1
             if(playerOneMoves == maxMoves){
 
                  return "Tie"
             }
         } else{
-            maxMoves = (gridSize * gridSize)/2 + 0.5
+            let maxMoves = (gridSize * gridSize)/2 + 0.5
             if(playerOneMoves == maxMoves){
 
                return "Tie"
