0d92c3fe53844555291b93013beb623293b1f959
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-07-21 20:05:25 -0500

    Redid the grid and created enumarete function

--- FBPort/static/js/main.js
+++ FBPort/static/js/main.js
@@ -1,6 +1,7 @@
 import {Gato} from "./gato.js"
 
 document.addEventListener('DOMContentLoaded', () => {
+    enumareteSquares()
     let gato = new Gato(3)
     window.onclick = event => {
         let element = event.target
@@ -8,4 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
             gato.markCell(element)
         }
     }
+
+function enumareteSquares(){
+   for(let row=0; row <= 9; row++){
+       let big_square = 0
+       for(let col=0; col <= 9; col++){
+        if(col % 3 == 0){ big_square++}
+        let small_square = document.getElementById(`${row}${col}`)
+        if(small_square == null){
+            console.log(`%{row}%{col}`)
+        }
+        small_square.id = `${big_square}${row}${col}`
+       }
+   }
+}
 })
--- FBPort/static/js/sudoku.js
+++ FBPort/static/js/sudoku.js
@@ -1,4 +1,18 @@
 
+let squares = document.getElementsByClassName('small_square')
+enumareteSquares(squares)
+
+function enumareteSquares(squares){
+    lastSquareId = parseInt(squares[squares.length -1].id)
+   for(row=0; row <= 9; row++){
+       big_square = 0
+       for(col=0; col <= 9; col++){
+        if(col % 3 == 0){ big_square++}
+        let small_square = document.getElementById(`%{row}%{col}`)
+        small_square.id = `%{big_square}%{row}%{col}`
+       }
+   }
+}
 
 class createSudoku{
 
