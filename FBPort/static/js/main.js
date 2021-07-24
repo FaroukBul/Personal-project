c4b548eebee92bbc021463edf2d9bc4e146241eb
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-07-23 19:07:46 -0500

    Now cuandrants are enumerated with integers

--- FBPort/static/js/main.js
+++ FBPort/static/js/main.js
@@ -13,22 +13,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
     function enumareteSquares(){
-        var abc = ["A", "B", "C"]
+        let big_square_row = 0
+        let square_end_row = 0
         for(let row=1; row <= 9; row++) {
             let big_square_col = 0
-            let square_end = 0
+            let square_end_col = 0
             for(let col=1; col <= 9; col++) {
                 let small_square = document.getElementById(`${row}${col}`)
-                small_square.id = `${abc[big_square_col]}${row}${col}`
+                small_square.id = `${big_square_row}${big_square_col}${row}${col}`
                 
                 if(col % 3 == 0){ 
-                    if(square_end == 3){
+                    if(square_end_col == 3){
                         big_square_col = 0
-                        square_end = 0
+                        square_end_col = 0
                     }
                     big_square_col++
-                    square_end++ 
+                    square_end_col++ 
                 }
+            }
+            if(row % 3 == 0){ 
+                if(square_end_row == 3){
+                    big_square_row = 0
+                    square_end_row = 0
+                }
+                big_square_row++
+                square_end_row++ 
             }
         }
      }
