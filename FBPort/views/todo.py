6c885ad12c9ca139eb2e860a25e635b16f8693eb
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-08-12 19:11:50 -0500

    Added errors for empty values and delete view

--- FBPort/views/todo.py
+++ FBPort/views/todo.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template, request,
+    redirect, url_for, flash
 )
 from FBPort.models.todo import Todo
 
@@ -10,14 +11,35 @@ bp = Blueprint("todo", __name__,url_prefix="/todo")
 
 @bp.route("/todo", methods=("POST","GET"))
 def todo():
+    todos = Todo.query.all()
     if request.method == "POST":
         datetime_str = request.form["due_datetime"]
-        datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d")
-        todo = Todo(
-            activity=request.form["activity"],
-            due_datetime=datetime_obj
-        )
-        todo.add()
+        if not datetime_str:
+            flash("You need a completion date, otherwise you'll procrastinate")
+        else:
+            datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d")
+
+            todo = Todo(
+                activity=request.form["activity"],
+                due_datetime=datetime_obj
+            )
+            error = todo.request.add()
+            if not error:
+                return redirect(
+                    url_for("todo.todo")
+                )
+            flash(error)
+
     return render_template(
         "todo/todo.html",
+        todos=todos
     )
+
+
+@bp.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.get(todo_id)
+    todo.delete()
+    return redirect(
+            url_for("todo.todo")
+        )
