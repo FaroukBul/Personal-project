c412661a655547311cc94e08c1cb4ae245aca6fe
FaroukBul <farouk_jr@hotmail.com>
2021-08-17 19:19:39 -0500

    Added validation for invalidate past dates and call the repeat method

--- FBPort/views/todo.py
+++ FBPort/views/todo.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 from flask import (
     Blueprint, render_template, request,
@@ -12,18 +13,20 @@ bp = Blueprint("todo", __name__,url_prefix="/todo")
 @bp.route("/todo", methods=("POST","GET"))
 def todo():
     todos = Todo.query.all()
+    check_for_expired_date(todos)
     if request.method == "POST":
         datetime_str = request.form["due_datetime"]
         if not datetime_str:
             flash("You need a completion date, otherwise you'll procrastinate")
         else:
-            datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d")
-
+            datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M")
             todo = Todo(
                 activity=request.form["activity"],
-                due_datetime=datetime_obj
+                due_datetime=datetime_obj,
+                days=request.form["days"]
             )
             error = todo.request.add()
+            todo.repeat()
             if not error:
                 return redirect(
                     url_for("todo.todo")
@@ -36,6 +39,11 @@ def todo():
     )
 
 
+def check_for_expired_date(todos):
+    for todo in todos:
+        todo.check_for_expiration()
+
+
 @bp.route("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = Todo.get(todo_id)
@@ -43,3 +51,4 @@ def delete(todo_id):
     return redirect(
             url_for("todo.todo")
         )
+
